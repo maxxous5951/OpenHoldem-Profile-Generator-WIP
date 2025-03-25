64eed5135bcf382c70c57e3620f2533b6110e500
maxxous5951 <100084407+maxxous5951@users.noreply.github.com>
2025-03-25 20:09:20 +0100

    Create __init__.py

---
+++ generators/river/__init__.py
@@ -0,0 +1,7 @@
+"""
+Module de générateurs river pour OpenHoldem Profile Generator
+Contient les classes pour générer les différentes parties d'un profil river OpenHoldem
+"""
+
+# Export pour faciliter l'accès depuis l'extérieur
+from generators.river.river_generator import RiverProfileGenerator
