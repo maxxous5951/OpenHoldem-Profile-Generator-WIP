9585e1ba832dc394ad715d30a9036aecb1a69fe6
maxxous5951 <100084407+maxxous5951@users.noreply.github.com>
2025-03-25 10:03:23 +0100

    Create __init__.py

---
+++ generators/__init__.py
@@ -0,0 +1,10 @@
+"""
+Module de générateurs pour OpenHoldem Profile Generator
+Contient les classes pour générer les différentes parties d'un profil OpenHoldem
+"""
+
+# Import pour faciliter l'accès depuis l'extérieur
+from generators.preflop_generator import PreflopProfileGenerator
+from generators.flop_generator import FlopProfileGenerator
+from generators.turn_generator import TurnProfileGenerator
+from generators.river_generator import RiverProfileGenerator
