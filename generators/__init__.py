576e0340e7e567ffe39b7f22f6e129f50376f6f7
maxxous5951 <100084407+maxxous5951@users.noreply.github.com>
2025-03-25 20:15:58 +0100

    Update __init__.py

--- generators/__init__.py
+++ generators/__init__.py
@@ -4,7 +4,7 @@ Contient les classes pour générer les différentes parties d'un profil OpenHol
 """
 
 # Import pour faciliter l'accès depuis l'extérieur
-from generators.preflop_generator import PreflopProfileGenerator
-from generators.flop_generator import FlopProfileGenerator
-from generators.turn_generator import TurnProfileGenerator
-from generators.river_generator import RiverProfileGenerator
+from generators.preflop import PreflopProfileGenerator
+from generators.flop import FlopProfileGenerator
+from generators.turn import TurnProfileGenerator
+from generators.river import RiverProfileGenerator
