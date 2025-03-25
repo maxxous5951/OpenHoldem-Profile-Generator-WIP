68c38285924a673ecd44fdd75addc02ec20733b2
maxxous5951 <100084407+maxxous5951@users.noreply.github.com>
2025-03-25 19:25:04 +0100

    Create __init__.py

---
+++ generators/turn/__init__.py
@@ -0,0 +1,7 @@
+"""
+Module de générateurs turn pour OpenHoldem Profile Generator
+Contient les classes pour générer les différentes parties d'un profil turn OpenHoldem
+"""
+
+# Export pour faciliter l'accès depuis l'extérieur
+from generators.turn.turn_generator import TurnProfileGenerator
