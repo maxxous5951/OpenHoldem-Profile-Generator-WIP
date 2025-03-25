cef7f9e5a652d35361fd366fed4835a64f80afd1
maxxous5951 <100084407+maxxous5951@users.noreply.github.com>
2025-03-25 18:40:13 +0100

    Update __init__.py

--- generators/flop/__init__.py
+++ generators/flop/__init__.py
@@ -1,1 +1,8 @@
 
+"""
+Module de générateurs flop pour OpenHoldem Profile Generator
+Contient les classes pour générer les différentes parties d'un profil flop OpenHoldem
+"""
+
+# Export pour faciliter l'accès depuis l'extérieur
+from generators.flop.flop_generator import FlopProfileGenerator
